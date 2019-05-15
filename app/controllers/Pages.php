ec95aad3ed327b25fce756a0fb5133db68f229e0
linpohsien <a4839500@gmail.com>
2019-05-15 22:00:08 +0800

    18

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,8 +1,14 @@
 <?php
-  class Pages {
+  class Pages extends Controller {
     public function __construct(){
+
     }
+
+    public function index(){
+      $this->view('hello');
+    }
+
     public function about($id){
-        echo $id;
+      echo $id;
     }
   }
